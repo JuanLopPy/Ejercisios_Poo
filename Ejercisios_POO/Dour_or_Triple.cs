0a45d2d548396f8f365aaa2aa220185f0560f4f1
juanm <juanm@JUANMA>
2023-09-04 13:00:51 -0500

    second exercise

---
+++ Ejercisios_POO/Dour_or_Triple.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Ejercisios_POO
+{
+    internal class Dour_or_Triple
+    {
+        #region properties 
+        public double num1 { get; set; }
+        public double num2 { get; set; }
+
+
+
+        #endregion
+
+        #region methods
+        public void IngresarNumeros()
+        {
+            Console.Write("Ingresa un número: ");
+            num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Ingresa otro número: ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public void Calcular_numero()
+        {
+            if (num1>=num2)
+            {
+
+                Console.WriteLine("El resultado es: " + num1 * 2);
+            
+            }
+            else { Console.WriteLine("El resultado es: " + num2 * 3); }
+
+        }
+
+
+
+
+        #endregion
+
+    }
+}
--- Ejercisios_POO/Program.cs
+++ Ejercisios_POO/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Ejercisios;
+using Ejercisios_POO;
 using System;
 using System.Collections.Generic;
 
@@ -8,13 +9,31 @@ namespace MyfirstOOPcode_saturday
     {
         static void Main(string[] args)
         {
-
+            // primer ejercisio 
+           /* Console.Write("Possitive power.\n");
             Possitive_Power cuadradoPositivo = new Possitive_Power();
             cuadradoPositivo.IngresarNumero();
-            cuadradoPositivo.CalcularCuadrado();
+            cuadradoPositivo.CalcularCuadrado(); */
+            //segundo ejercisio 
+            Console.Write("Possitive power.\n");
+            Dour_or_Triple dour_Or_Triple = new Dour_or_Triple();
+            dour_Or_Triple.IngresarNumeros();
+            dour_Or_Triple.Calcular_numero();
+
+
+
+
 
 
         }
+
+
+
+
+
+
+
+
     }
 
 

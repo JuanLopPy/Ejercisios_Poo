6479bfbf90fa15c9f644616fb6cd9b1878e7c080
juanm <juanm@JUANMA>
2023-09-04 08:19:27 -0500

    continue exercises

--- Ejercisios_POO/Possitive_power.cs
+++ Ejercisios_POO/Possitive_power.cs
@@ -3,10 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
-namespace Ejercisios_POO
+namespace Ejercisios
 {
-    internal class Possitive_power
+    internal class Possitive_Power
     {
+        #region properties
+        public double numero { get; set; }
+        #endregion
+
+
+        #region methods
+        public void IngresarNumero()
+        {
+            Console.Write("Ingresa un número: ");
+            numero = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public void CalcularCuadrado()
+        {
+            if (numero > 0)
+            {
+                double resultado = Math.Pow(numero, 2);
+                Console.WriteLine($"El cuadrado del número {numero} es {resultado}");
+            }
+            else
+            {
+                Console.WriteLine("El número ingresado no es positivo.");
+            }
+        }
     }
+
+
+    #endregion
+
 }
+
--- Ejercisios_POO/Program.cs
+++ Ejercisios_POO/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Ejercisios;
+using System;
+using System.Collections.Generic;
+
+namespace MyfirstOOPcode_saturday
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Possitive_Power cuadradoPositivo = new Possitive_Power();
+            cuadradoPositivo.IngresarNumero();
+            cuadradoPositivo.CalcularCuadrado();
+
+
+        }
+    }
+
+
+}

b805b551e635fc3ee58f18c1398af01e2a0c99d4
juanm <juanm@JUANMA>
2023-09-06 15:12:24 -0500

    Smallest of Five. complete

--- Ejercisios_POO/Program.cs
+++ Ejercisios_POO/Program.cs
@@ -10,88 +10,95 @@ namespace MyfirstOOPcode_saturday
     {
         static void Main(string[] args)
         {
-           // first exercise 
-           /*
-           Console.Write("Possitive power.\n");
-            Possitive_Power cuadradoPositivo = new Possitive_Power();
-            cuadradoPositivo.IngresarNumero();
-            cuadradoPositivo.CalcularCuadrado(); 
-            //second exercise
+            // first exercise 
+            /*
+            Console.Write("Possitive power.\n");
+             Possitive_Power cuadradoPositivo = new Possitive_Power();
+             cuadradoPositivo.IngresarNumero();
+             cuadradoPositivo.CalcularCuadrado(); 
+             //second exercise
 
-            Console.Write("Dour or triple.\n");
-            Dour_or_Triple dour_Or_Triple = new Dour_or_Triple();
-            dour_Or_Triple.IngresarNumeros();
-            dour_Or_Triple.CalcularNumero();
-            // thyrd exercise 
+             Console.Write("Dour or triple.\n");
+             Dour_or_Triple dour_Or_Triple = new Dour_or_Triple();
+             dour_Or_Triple.IngresarNumeros();
+             dour_Or_Triple.CalcularNumero();
+             // thyrd exercise 
 
-            Console.Write("Root or Square.\n");
-            Console.Write("Ingresa un número: ");
-            double numero = Convert.ToDouble(Console.ReadLine());
-            Root_or_Square root= new Root_or_Square(numero);
-            root.CalcularNumero();
+             Console.Write("Root or Square.\n");
+             Console.Write("Ingresa un número: ");
+             double numero = Convert.ToDouble(Console.ReadLine());
+             Root_or_Square root= new Root_or_Square(numero);
+             root.CalcularNumero();
 
-            // fourth exercise
-            Console.Write("Circle Perimeter.\n");
-            Console.Write("Ingresa el radio del circulo: ");
-            double radio = Convert.ToDouble(Console.ReadLine());
-            Circle_Perimeter circle = new Circle_Perimeter();
-            circle.radio=radio;
-            circle.CalcularPerimetro(); 
+             // fourth exercise
+             Console.Write("Circle Perimeter.\n");
+             Console.Write("Ingresa el radio del circulo: ");
+             double radio = Convert.ToDouble(Console.ReadLine());
+             Circle_Perimeter circle = new Circle_Perimeter();
+             circle.radio=radio;
+             circle.CalcularPerimetro(); 
+
+
+             //fifth exercise
+             Console.Write("Midweek day.\n");
+             Console.Write("Ingresa un número entre 1 y 7 : ");
+             int num = Convert.ToInt32(Console.ReadLine());
+            Midweek_Day midweek_Day = new Midweek_Day();
+             midweek_Day.num=num;  
+            midweek_Day.CalcularDia(); 
+             // sixth exercise 
+             Console.Write("Tax Calculator.\n");
+             Console.Write("Ingresa tu salario: ");
+             decimal salario = Convert.ToDecimal(Console.ReadLine());
+             Tax_Calculator tax_Calculator = new Tax_Calculator();
+             tax_Calculator.salarioAnual = salario;
+             tax_Calculator.calcularSalario();
+
+             // seventh execise
+             Console.Write("Tax Calculator.\n");
+             Console.Write("Ingresa un número: ");
+             double numero1 = Convert.ToDouble(Console.ReadLine());
+             Console.Write("Ingresa otro número: ");
+             double num2 = Convert.ToDouble(Console.ReadLine());
+             Remainder_Finder remainder_Finder = new Remainder_Finder(); 
+             remainder_Finder.num1=numero1;
+             remainder_Finder.num2=num2;
+             remainder_Finder.calcularResiduo();
+              // eighth exercise
+              Sum_Evens sum_Evens = new Sum_Evens();
+             sum_Evens.suma();
+
+             // nineth exercise
+             Console.Write("Fracction diference.\n");
+             Console.Write("Ingresa la primera fracción (numerador/denominador): ");
+             string fraccion1 = Console.ReadLine();
+             Console.Write("Ingresa la segunda fracción (numerador/denominador): ");
+             string fraccion2 = Console.ReadLine();
+             Fraction_Difference fraction_Difference = new Fraction_Difference();
+             fraction_Difference.fraccion1 = fraccion1;
+             fraction_Difference.fraccion2 = fraccion2;
+             fraction_Difference.mostrar();
+             // tenth exercise
+             Console.Write("String Length. \n ");
+             Console.Write("Ingrese una palabra: ");
+             string palabra = Console.ReadLine();
+             String_Length string_Length = new String_Length();
+             string_Length.palabra = palabra;
+             Console.Write("" +string_Length.NumPalabra()); 
             
-
-            //fifth exercise
-            Console.Write("Midweek day.\n");
-            Console.Write("Ingresa un número entre 1 y 7 : ");
-            int num = Convert.ToInt32(Console.ReadLine());
-           Midweek_Day midweek_Day = new Midweek_Day();
-            midweek_Day.num=num;  
-           midweek_Day.CalcularDia(); 
-            // sixth exercise 
-            Console.Write("Tax Calculator.\n");
-            Console.Write("Ingresa tu salario: ");
-            decimal salario = Convert.ToDecimal(Console.ReadLine());
-            Tax_Calculator tax_Calculator = new Tax_Calculator();
-            tax_Calculator.salarioAnual = salario;
-            tax_Calculator.calcularSalario();
-
-            // seventh execise
-            Console.Write("Tax Calculator.\n");
-            Console.Write("Ingresa un número: ");
-            double numero1 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Ingresa otro número: ");
-            double num2 = Convert.ToDouble(Console.ReadLine());
-            Remainder_Finder remainder_Finder = new Remainder_Finder(); 
-            remainder_Finder.num1=numero1;
-            remainder_Finder.num2=num2;
-            remainder_Finder.calcularResiduo();
-             // eighth exercise
-             Sum_Evens sum_Evens = new Sum_Evens();
-            sum_Evens.suma();
-
-            // nineth exercise
-            Console.Write("Fracction diference.\n");
-            Console.Write("Ingresa la primera fracción (numerador/denominador): ");
-            string fraccion1 = Console.ReadLine();
-            Console.Write("Ingresa la segunda fracción (numerador/denominador): ");
-            string fraccion2 = Console.ReadLine();
-            Fraction_Difference fraction_Difference = new Fraction_Difference();
-            fraction_Difference.fraccion1 = fraccion1;
-            fraction_Difference.fraccion2 = fraccion2;
-            fraction_Difference.mostrar();
-            // tenth exercise
-            Console.Write("String Length. \n ");
-            Console.Write("Ingrese una palabra: ");
-            string palabra = Console.ReadLine();
-            String_Length string_Length = new String_Length();
-            string_Length.palabra = palabra;
-            Console.Write("" +string_Length.NumPalabra()); 
-           */
             // eleventh exercise 
-          Average_of_Four average = new Average_of_Four();
+            Console.Write("Average of four. \n ");
+            Average_of_Four average = new Average_of_Four();
             average.IngresaNum();
-            average.prom();
+            average.prom(); */
 
-            
+            // twelve exercise 
+            Console.Write(" Smallest of Five. \n ");
+            Smallest_of_five smallest= new Smallest_of_five();
+            smallest.IngresaNum();
+            smallest.minimo();  
+
+
 
         }
 
---
+++ Ejercisios_POO/Smallest_of_five.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercisios_POO
+{
+    internal class Smallest_of_five
+    {
+
+        #region properties
+        int[] vec = new int[5];
+
+        #endregion
+        #region methods
+        public void IngresaNum()
+        {
+
+            for (int i = 0; i < vec.Length; i++)
+            {
+                Console.WriteLine($"ingrese un número :");
+                vec[i] = int.Parse(Console.ReadLine());
+            }
+
+
+        }
+        public void minimo()
+        {
+            double minimo= vec[0];
+            for (int i = 0; i < vec.Length; i++)
+            {
+                if (vec[i] < minimo)
+                {
+                    minimo = vec[i];
+
+                }
+
+            }
+            Console.WriteLine("El valor mínimo es: " + minimo);
+
+        }
+
+        #endregion
+    }
+}
